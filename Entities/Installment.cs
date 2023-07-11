a1b88cb890d4ea1253391e532e3b01b34eed3fdb
JuanAsbahr <juan_asbahr@hotmail.com>
2023-07-11 15:54:51 -0300

    Entitie - Installment

---
+++ Entities/Installment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Contract_Payment.Entities
+{
+    internal class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+        public override string ToString()
+        {
+            return DueDate.ToString("dd/MM/yyyy")
+                + " - "
+                + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        internal static void Add(Installment installment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
